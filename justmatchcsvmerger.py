d976b6cbaa1c4291d1f36090676cc4eb42ec8458
robustgarlic <60711968+robustgarlic@users.noreply.github.com>
2020-05-04 10:36:02 -0400

    Update justmatchcsvmerger.py

--- justmatchcsvmerger.py
+++ justmatchcsvmerger.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-# Read the files into two dataframes.
-df1 = pd.read_csv('t1s.csv')
-df2 = pd.read_csv('tomhunter.csv')
 
-# Merge the two dataframes, using _ID column as key
-df3 = pd.merge(df1, df2, on = 'circuitid')
-df3.set_index('circuitid', inplace = True)
+df1 = pd.read_csv('csv1.csv')
+df2 = pd.read_csv('csv2r.csv')
+
+
+df3 = pd.merge(df1, df2, on = 'firstcolumnheader')
+df3.set_index('firstcoulumnheader', inplace = True)
 
 # Write it to a new CSV file
 df3.to_csv('resultsthatmatch.csv')
