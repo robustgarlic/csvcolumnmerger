9229eb8d709372f173977029c3869dd1e557f9b1
Nick <60711968+robustgarlic@users.noreply.github.com>
2021-03-14 12:22:35 -0400

    Update pandasmergecsvmerger.py

--- pandasmergecsvmerger.py
+++ pandasmergecsvmerger.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-df1 = pd.read_csv("t1s.csv")
-df2 = pd.read_csv("tomhunter.csv")
-merged = df1.merge(df2, on="circuitid", how="outer").fillna("-")
+df1 = pd.read_csv("firstfile.csv")
+df2 = pd.read_csv("secondfile.csv")
+merged = df1.merge(df2, on="header", how="outer").fillna("-")
 merged.to_csv("merged.csv", index=False)
